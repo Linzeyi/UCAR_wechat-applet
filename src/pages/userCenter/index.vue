53b5f3dad9b7ed65352f55bd7b808bda9bcaaddc
伍杰麟 <jielin.wu@ucarinc.com>
2019-09-04 23:40:13 +0800

    完成个人中心页面和充值页面

--- src/pages/userCenter/index.vue
+++ src/pages/userCenter/index.vue
@@ -1,17 +1,157 @@
 <template>
   <div class="userCenter-wrap">
-    个人中心
+    <div class="user-info">
+      <div>
+        <img src="../../../static/images/user.png" alt="" class="user-icon">
+      </div>
+      <div class="info">
+        <div class="user-name">
+          <p>{{ accountName }}</p><span>{{ accountLevel }}</span>
+        </div>
+        <p>积分：{{ integral }}</p>
+      </div>
+      <i class="iconfont setting">&#xe61f;</i>
+    </div>
+    <div class="phone">
+      <p>{{ encodePhone }}</p>
+    </div>
+    <div class="weui-cells">
+      <div class="weui-cell" @click="routeTo('wallet')">
+        <div class="weui-cell__hd">
+          <i class="iconfont">&#xe697;</i>
+        </div>
+        <div class="weui-cell__bd">
+          <p>我的钱包</p>
+        </div>
+        <span class="weui-cell__ft">
+          <p>{{ balance }}元<i class="iconfont">&#xe601;</i></p>
+        </span>
+      </div>
+      <div class="weui-cell">
+        <div class="weui-cell__hd">
+          <i class="iconfont">&#xe619;</i>
+        </div>
+        <div class="weui-cell__bd">
+          <p>我的订单</p>
+        </div>
+        <span class="weui-cell__ft">
+          <p>{{ orderNum }}条<i class="iconfont">&#xe601;</i></p>
+        </span>
+      </div>
+      <div class="weui-cell">
+        <div class="weui-cell__hd">
+          <i class="iconfont">&#xe605;</i>
+        </div>
+        <div class="weui-cell__bd">
+          <p>地址管理</p>
+        </div>
+        <span class="weui-cell__ft">
+          <i class="iconfont">&#xe601;</i>
+        </span>
+      </div>
+      <div class="weui-cell">
+        <div class="weui-cell__hd">
+          <i class="iconfont">&#xe6bb;</i>
+        </div>
+        <div class="weui-cell__bd">
+          <p>我的消息</p>
+        </div>
+        <span class="weui-cell__ft">
+          <p>{{ messageNum }}条<i class="iconfont">&#xe601;</i></p>
+        </span>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
+  data () {
+    return {
+      accountName: 'ThO', // 用户名
+      accountLevel: '钻石', // 账户星级
+      integral: 135, // 积分
+      phone: '13015768195', // 手机号码
+      balance: '100.00', // 余额
+      orderNum: 20, // 订单数
+      messageNum: 2 // 消息数
+    }
+  },
   created () {
 
+  },
+  computed: {
+    encodePhone () {
+      var str = this.phone.slice(0, 3) + '****' + this.phone.slice(7)
+      return str
+    }
+  },
+  methods: {
+    // 跳转充值页面
+    routeTo (type) {
+      switch (type) {
+        case 'wallet':
+          mpvue.navigateTo({ url: '/pages/wallet/main' })
+          break
+      }
+    }
   }
 }
 </script>
 
-<style>
-
+<style lang="less" scoped>
+.user-info {
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  .user-icon {
+    height: 80px;
+    width: 80px;
+    flex: 5;
+    padding: 0 5px;
+    border-radius: 15px;
+  }
+  .info {
+    flex: 3;
+    & > p {
+      font-size: 0.3rem;
+    }
+    .user-name {
+      display: flex;
+      span {
+        border: 0.5px solid rgb(247, 97, 97);
+        border-radius:5px;
+        background-color: rgb(255, 171, 171);
+        padding: 0 8px;
+        margin-left: 18px;
+        font-size: 0.25rem;
+        align-self: center;
+        color: rgb(253, 77, 77);
+        line-height: inherit;
+      }
+    }
+  }
+  .setting {
+    font-size: 0.6rem;
+    flex: 0.7;
+  }
+}
+.phone {
+  margin-left: 15px;
+  p {
+    font-size: 0.3rem;
+  }
+}
+.weui-cells {
+  p {
+    padding: 5px 0;
+  }
+  .weui-cell__hd .iconfont {
+    font-size: 0.5rem;
+    margin-right: 9px;
+  }
+  .weui-cell__ft .iconfont {
+    font-size: 0.3rem;
+  }
+}
 </style>
