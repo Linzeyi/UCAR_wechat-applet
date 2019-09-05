81ad41856bbe676d0395e270e562f37bbf24efcc
林泽毅 <zeyi.lin@ucarinc.com>
2019-09-05 13:46:26 +0800

    goodsDetail

--- src/components/goodsDetail/goodsComment/goodsComment.vue
+++ src/components/goodsDetail/goodsComment/goodsComment.vue
@@ -10,7 +10,9 @@
         <div class="right-box">
           <div class="progress-box">
             <div class="progress-panel" v-for="(item, index) in scoreList" :key="index">
-              <div class="star-box"></div>
+              <div class="star-box">
+                <i class="iconfont icon-star" v-for="(scoreItem, scoreIndex) in item.score" :key="scoreIndex">&#xe663;</i>
+              </div>
               <div class="progress">
                 <progress :percent="(item.num / totalComment) * 100" color="#666" stroke-width="4"></progress>
               </div>
@@ -34,24 +36,24 @@ export default {
       totalComment: 1253,
       scoreList: [
         {
-          score: 1,
-          num: 10
-        },
-        {
-          score: 2,
-          num: 67
-        },
-        {
-          score: 3,
-          num: 125
+          score: 5,
+          num: 549
         },
         {
           score: 4,
           num: 502
         },
         {
-          score: 5,
-          num: 549
+          score: 3,
+          num: 125
+        },
+        {
+          score: 2,
+          num: 67
+        },
+        {
+          score: 1,
+          num: 10
         }
       ]
     }
@@ -103,7 +105,20 @@ export default {
               margin: 0;
             }
             .star-box {
-              width: 80px;
+              height: 4px;
+              line-height: 0px;
+              padding-right: 8px;
+              width: 60px;
+              text-align: right;
+              .icon-star {
+                font-size: 12px;
+                margin-right: 4px;
+                display: inline-block;
+                width: 4px;
+                height: 4;
+                line-height: 4px;
+                color: #666;
+              }
             }
             .progress {
               flex-grow: 1;
