63f98c2e2032036bdc4b591edabd49976d526034
hjs1070180527 <1070180527@qq.com>
2019-09-06 23:05:26 +0800

    分类页面

--- src/pages/classification/index.vue
+++ src/pages/classification/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="wrap">
-    <div class="left-tab">
+    <div class="left-panel">
       <div
         :class="{active: isActive === idx}"
         @click="isActive = idx"
@@ -8,7 +8,9 @@
         :key="item.id"
       ><span>{{item.name}}</span></div>
     </div>
-    <div class="right-tab"></div>
+    <div class="right-panel" >
+      <img  v-for="item in images" :src="item.url" :key="item.id">
+    </div>
   </div>
 </template>
 
@@ -22,6 +24,32 @@ export default {
         { id: '2', name: '品类二' },
         { id: '3', name: '品类三' },
         { id: '4', name: '品类四' }
+      ],
+      images: [
+        {id: '1', url: ''},
+        {id: '2', url: ''},
+        {id: '3', url: ''},
+        {id: '4', url: ''},
+        {id: '5', url: ''},
+        {id: '6', url: ''},
+        {id: '7', url: ''},
+        {id: '8', url: ''},
+        {id: '9', url: ''},
+        {id: '10', url: ''},
+        {id: '11', url: ''},
+        {id: '12', url: ''},
+        {id: '13', url: ''},
+        {id: '14', url: ''},
+        {id: '15', url: ''},
+        {id: '16', url: ''},
+        {id: '17', url: ''},
+        {id: '18', url: ''},
+        {id: '19', url: ''},
+        {id: '20', url: ''},
+        {id: '21', url: ''},
+        {id: '22', url: ''},
+        {id: '23', url: ''},
+        {id: '24', url: ''}
       ]
     };
   }
@@ -31,20 +59,22 @@ export default {
 <style lang="less" scoped>
 .active {
   background-color: rgb(200, 200, 200);
-  color: aquamarine;
+  color: rgb(26,188,156);
 }
 
 .wrap {
   width: 100%;
   height: 100%;
+  display: flex;
 }
 
-.left-tab {
+.left-panel {
   width: 120px;
   height: 100%;
   text-align: center;
   border-right: 1px solid rgb(200, 200, 200);
   overflow: scroll;
+  flex-shrink: 0;
 
   > div {
     height: 40px;
@@ -53,6 +83,20 @@ export default {
     > span {
       line-height: 40px;
     }
+  }
+}
+
+.right-panel {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+  align-content: flex-start;
+
+  img {
+    height: 70px;
+    width: 70px;
+    margin: 10px;
+    border: 1px solid black;
   }
 }
 </style>
