f3d5f62e058adf23e6c40e2f57aed586a4b24a00
hjs1070180527 <1070180527@qq.com>
2019-09-21 15:25:09 +0800

    前端不判断url是否需要token，后端判断，响应拦截器根据code跳转登录

--- src/api/config.js
+++ src/api/config.js
@@ -7,12 +7,6 @@ const config = {
   Auth: {
     cid: '007001',
     accountKey: '7vL5EioNUyQ9Rs5gv7Jm'
-  },
-  AccessUrl: [
-    '/action/user/login',
-    '/action/user/register',
-    '/action/user/getCaptcha',
-    '/action/goods/getPopularSearch'
-  ]
+  }
 }
 module.exports = config
